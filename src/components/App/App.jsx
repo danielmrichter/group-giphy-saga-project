9327f0934b528a48ad9cbdbc7100c47086888591
Ben Pollack <benpollack8723@gmail.com>
2024-08-23 10:30:50 -0500

    styling of search and header and nav

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,12 @@
 import { HashRouter as Router, Route, Link } from "react-router-dom";
 import Search from "../Search/Search.jsx";
 import Favorites from "../Favorites/Favorites";
+import Header from "../Header/Header.jsx"
 
 function App() {
   return (
     <Router>
+      <Header />
       <Route exact path="/">
         <Search />
       </Route>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,32 @@
+import { Link, useLocation } from "react-router-dom";
+import { Button, Box, Typography } from "@mui/material";
 
-
-const Header () {
-
+function Header () {
+  const location = useLocation();
+  const currentPath = location.pathname
+ const selectLink= () => {
+  if(currentPath === '/'){
+    return(
+    <Link to="/favorites">Favorites</Link>)
+  }
+  else if(currentPath === '/favorites'){
+    return(
+    <Link to="/">Search</Link>)
+  }
+ }
 
     return(
-        <div>
-          <h1>Giphy Search!</h1>
-        </div>
+        <Box 
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        paddingTop={4}
+        paddingBottom={4}
+        marginBottom={4}
+        bgcolor="#16a085">
+          <Typography fontFamily="sans-serif" fontSize={58} style={{color: "lightgray"}}>Giphy Search!</Typography>
+          <Button variant="contained">{selectLink()}</Button>
+        </Box>
     )
 }
 
